92c8b7b8342981e27cdc1cffd3444347c0be31a0
megahomyak <g.megahomyak@gmail.com>
2022-09-18 12:05:02 +0500

    Added the "Saved Suggestions" route

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
 void main() {
-  runApp(const MyApp());
+    runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+const title = 'Startup Name Generator';
 
-  @override
-  Widget build(BuildContext context) {
-    const title = 'Startup Name Generator';
-    return MaterialApp(
-      title: title,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(title),
-        ),
-        body: const Center(
-          child: RandomWords(),
-        ),
-      ),
-    );
-  }
+class MyApp extends StatelessWidget {
+    const MyApp({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            title: title,
+            home: RandomWords(),
+        );
+    }
 }
 
 class RandomWords extends StatefulWidget {
@@ -39,46 +33,89 @@ class _RandomWordsState extends State<RandomWords> {
 
     @override
     Widget build(BuildContext context) {
-        return ListView.builder(
-            padding: const EdgeInsets.all(16.0),
-            itemBuilder: (context, i) {
-                if (i.isOdd) {
-                    return SizedBox(height: 10);
-                }
-                i = i ~/ 2;
-                if (i >= _suggestions.length) {
-                    _suggestions.addAll(generateWordPairs().take(10));
-                }
-                final alreadySaved = _saved.contains(_suggestions[i]);
-                return Card(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(30)),
-                    ),
-                    child: ListTile(
-                        title: Text(
-                            _suggestions[i].asPascalCase,
-                            style: _biggerFont,
+        return Scaffold(
+            appBar: AppBar(
+                title: const Text(title),
+                actions: [
+                    IconButton(
+                        icon: const Icon(Icons.list),
+                        onPressed: _pushSaved,
+                        tooltip: "Saved Suggestions",
+                    )
+                ]
+            ),
+            body: ListView.builder(
+                padding: const EdgeInsets.all(16.0),
+                itemBuilder: (context, i) {
+                    if (i.isOdd) {
+                        return SizedBox(height: 10);
+                    }
+                    i = i ~/ 2;
+                    if (i >= _suggestions.length) {
+                        _suggestions.addAll(generateWordPairs().take(10));
+                    }
+                    final alreadySaved = _saved.contains(_suggestions[i]);
+                    return Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
                         ),
-                        trailing: IconButton(
-                            icon: Icon(
-                                alreadySaved ? Icons.favorite : Icons.favorite_border,
-                                color: alreadySaved ? Colors.red : null,
-                                semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+                        child: ListTile(
+                            title: Text(
+                                _suggestions[i].asPascalCase,
+                                style: _biggerFont,
                             ),
-                            onPressed: () {
-                                setState(() {
-                                    if (alreadySaved) {
-                                        _saved.remove(_suggestions[i]);
-                                    } else {
-                                        _saved.add(_suggestions[i]);
-                                    }
-                                });
-                            }
-                        )
-                    ),
-                    elevation: 10,
-                );
-            }
+                            trailing: IconButton(
+                                icon: Icon(
+                                    alreadySaved ? Icons.favorite : Icons.favorite_border,
+                                    color: alreadySaved ? Colors.red : null,
+                                    semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+                                ),
+                                onPressed: () {
+                                    setState(() {
+                                        if (alreadySaved) {
+                                            _saved.remove(_suggestions[i]);
+                                        } else {
+                                            _saved.add(_suggestions[i]);
+                                        }
+                                    });
+                                }
+                            )
+                        ),
+                        elevation: 10,
+                    );
+                }
+            )
+        );
+    }
+
+    void _pushSaved() {
+        Navigator.of(context).push(
+            MaterialPageRoute<void>(
+                builder: (context) {
+                    final tiles = _saved.map(
+                        (pair) {
+                            return ListTile(
+                                title: Text(
+                                    pair.asPascalCase,
+                                    style: _biggerFont,
+                                )
+                            );
+                        }
+                    );
+                    final divided = tiles.isNotEmpty
+                        ? ListTile.divideTiles(
+                              context: context,
+                              tiles: tiles,
+                          ).toList()
+                        : <Widget>[];
+                    return Scaffold(
+                        appBar: AppBar(
+                            title: const Text("Saved Suggestions")
+                        ),
+                        body: ListView(children: divided)
+                    );
+                }
+            )
         );
     }
 }
